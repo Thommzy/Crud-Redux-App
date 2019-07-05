a2718f5550a04ff3d98dcfb06e2f309475f3d644
Thommzy <tomiwaobeisun@yahoo.com>
2019-07-05 18:23:16 +0100

    changes

---
+++ src/Features/Event/EventList/EventListItem.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
+import EventListAtendee from "./EventListAtendee";
+
+class EventListItem extends Component {
+  render() {
+    const { event, selectEvent, deleteEvent } = this.props;
+    return (
+      <Segment.Group>
+        <Segment>
+          <Item.Group>
+            <Item>
+              <Item.Image size='tiny' circular src={event.hostPhotoURL} />
+              <Item.Content>
+                <Item.Header>{event.title}</Item.Header>
+                <Item.Description>Hosted by {event.hostedBy}</Item.Description>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+        <Segment>
+          <span>
+            <Icon name='clock' /> {event.date} |
+            <Icon name='marker' /> {event.venue}
+          </span>
+        </Segment>
+        <Segment secondary>
+          <List horizontal>
+            {event.attendees &&
+              event.attendees.map(attendee => (
+                <EventListAtendee key={attendee.id} attendee={attendee} />
+              ))}
+          </List>
+        </Segment>
+        <Segment clearing>
+          <span>{event.description}</span>
+          <Button
+            onClick={evt => deleteEvent(event.id)}
+            as='a'
+            color='red'
+            floated='right'
+            content='Delete'
+          />
+          <Button
+            onClick={evt => selectEvent(evt, event)}
+            as='a'
+            color='teal'
+            floated='right'
+            content='View'
+          />
+        </Segment>
+      </Segment.Group>
+    );
+  }
+}
+
+export default EventListItem;
---
+++ src/app/layout/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from "react";
+//import { Button } from "semantic-ui-react";
+import EventDashboard from "../../Features/Event/EventDashboard/EventDashboard";
+import NavBar from "../../Features/Nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
